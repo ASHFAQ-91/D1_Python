ed173d14f6693fb42991bd706d70bc3a698d89f6
Ashfaq-91 <ashfaqqureshi242@gmail.com>
2026-07-18 08:59:16 +0400

    Add file - 4ArithmeticOptr.py

---
+++ Concept_1/4ArithmeticOptr.py
@@ -0,0 +1,10 @@
+# Types of Operator:-
+# 1. Arithmetic Optr -> add, subtract, multi, division, floar division, modules.
+
+print(5+3)      # add.
+print(10-4)     # subtract.
+print(6*7)      # multiplication.
+print(17/3)     # Division.
+print(17//3)    # Floor Division (Round down to integer).
+print(17%3)     # Modules (reminder).
+print(2**3)     # Exponent (we can say "Square").
