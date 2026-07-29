826acb5dcef379653595e77db3df6011ba4e1fa0
ASHFAQ-91 <ashfaqqureshi242@gmail.com>
2026-07-29 23:26:22 +0400

    Adding 17ForLoop.py

---
+++ Concept_1/17ForLoop.py
@@ -0,0 +1,18 @@
+# Types of Iteration/Loop in Python.
+# 1. For Loop.
+# 2. While Loop.
+
+#$ for string.
+# name = 'Ashfaq'
+# for char in name:
+#     print(char);
+
+#$ for List.
+colors = ['Red', 'Green', 'Blue']
+for i in colors:
+    print(i)
+
+for i in colors:
+    print("\n")
+    for j in i:
+        print(j)
