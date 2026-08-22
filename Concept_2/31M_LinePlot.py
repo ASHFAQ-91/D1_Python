370baf3d22a4b5056fea07b69fdc520dde150ae7
ASHFAQ-91 <ashfaqqureshi242@gmail.com>
2026-08-22 11:39:51 +0400

    Adding new program 31M_LinePlot.py

---
+++ Concept_2/31M_LinePlot.py
@@ -0,0 +1,16 @@
+# Matplotlib for Data Analytics.
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = [1, 2, 3, 4, 5]             # datatype is list
+y = [10, 20, 15, 30, 25]        # datatype is list
+
+plt.plot(x, y, color='red', linestyle='--', linewidth=2, marker='o', markersize=8)
+plt.title("Simple Line Plot")
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.show()
+
+
+#Q. In this program, why we uaed numpy library?
+#A. In this specific code snippet, the `numpy` library is imported but not actually used. The import statement for `numpy` is unnecessary in this case, as the code only utilizes lists for the x and y values in the line plot. The `numpy` library is typically used for numerical operations and array manipulations, but since the code does not perform any such operations, it can be removed without affecting the functionality of the line plot.
